ade78a8fd5f25648c7ac465f1d3afbe01c56ead8
Guido van Rossum <guido@python.org>
2026-03-01 22:33:19 -0800

    Ready for the 21st century (waiting for SSL cert though)

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # pythonlabs.com
 Source code for pythonlabs.com
 
-# running
-- install Google Cloud Platform (Google that for instructions)
-- `gcloud app deploy`
+# Prerequisites
+- Install the [Google Cloud SDK](https://cloud.google.com/sdk/docs/install)
+- Authenticate: `gcloud auth login`
+- Set your project: `gcloud config set project <YOUR_PROJECT_ID>`
+
+# Local testing
+```
+pip install -r requirements.txt
+python main.py
+```
+Then visit http://127.0.0.1:8080
+
+# Deploying
+```
+gcloud app deploy
+```
--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-import os
 import logging
 
-def main():
-  path = os.environ["PATH_INFO"]
-  qs = os.getenv("QUERY_STRING")
-  if qs:
-    path += "?" + qs
-  referer = os.environ.get("HTTP_REFERER")
-  if referer:
-    logging.info("Request %s from %s", path, referer)
-  else:
-    logging.info("Request %s", path)
-  for key, value in sorted(os.environ.iteritems()):
-    logging.debug("%s=%r", key, value)
-  print "Status: 404 Not found"
+from flask import Flask, request
 
-if __name__ == '__main__':
-  main()
+app = Flask(__name__)
+
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def catch_all(path):
+    full_path = request.full_path.rstrip("?")
+    referer = request.headers.get("Referer")
+    if referer:
+        logging.info("Request %s from %s", full_path, referer)
+    else:
+        logging.info("Request %s", full_path)
+    return "Not found", 404
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
